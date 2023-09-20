5617c99c1618908d936ad1e7d4621ec95133ee76
michaelbir <mikux22@gmail.com>
2023-09-20 20:30:10 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//console.log ("Hello World!");
+
+let firstName = 'Harry';
+let lastName = 'Potter';
+
+//console.log ("Hello, " + firstName + "!");
+
+let greeting = (`Hello, ${firstName} ${lastName}! `);
+console.log(greeting);
